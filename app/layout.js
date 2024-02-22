3c09016dc99dd58e55f3c781c6e62a044ff2c16a
rachitkumar205 <rk_programmer@outlook.com>
2024-02-22 14:23:47 +0530

    made business card

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Cormorant_Garamond } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Cormorant_Garamond({ subsets: ["latin"], weight: "500" });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-white h-screen w-screen`}>{children}</body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        bone: '#e3dac9',
+      },
     },
   },
   plugins: [],
